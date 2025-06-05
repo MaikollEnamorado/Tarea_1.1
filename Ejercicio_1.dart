a583d00f695f64d4349fc89b78cca3fa0bef1798
Maikoll Enamorado <maikoll.enamorado@unah.hn>
2025-06-04 22:43:53 -0600

    Ejercio_1 completado

--- Ejercicio_1.dart
+++ Ejercicio_1.dart
@@ -1,3 +1,12 @@
 void main(){
+  final int cantidadTerminos = 15;
   
+  final List<int> sucesionFibonacci = [0,1];
+
+  for( int i = 2; i < cantidadTerminos; i++){
+    int siguienteNumero = sucesionFibonacci[i-1] + sucesionFibonacci[i-2];
+    sucesionFibonacci.add(siguienteNumero);
+  }
+
+  print('La lista de los primeros $cantidadTerminos numeros de Fibonacci es: $sucesionFibonacci');
 }
