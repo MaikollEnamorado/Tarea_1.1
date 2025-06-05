21da78c0b4291766d53f8022e53b2096666df1b7
Maikoll Enamorado <maikoll.enamorado@unah.hn>
2025-06-05 10:32:39 -0600

    Ejercio_3 completado

--- Ejercicio_3.dart
+++ Ejercicio_3.dart
@@ -1,4 +1,28 @@
 import 'Empleado.dart' show Empleado;
 void main(){
+  final List<Empleado> empleados = [];
+  empleados.add(Empleado(nombre: "Juan Perez", puesto: "Gerente", salario: 30000));
+  empleados.add(Empleado(nombre: "Pedro Cruz", puesto: "Supervisor", salario: 20000));
+  empleados.add(Empleado(nombre: "Luiz Martinez", puesto: "Operario", salario: 10000));
+  
+  final double total = salarioTotal(empleados);
+  final double promedio = salarioPromedio(empleados);
+  
+  print("Salario total de todos los empleados: $total");
+  print("Salario promedio de todos los empleados: $promedio");
+}
+double salarioTotal(List<Empleado> empleados){
+  double total = 0;
 
+  for(final empleado in empleados){
+    total += empleado.salario;
+  }
+
+  return total;
+}
+double salarioPromedio(List<Empleado> empleados){
+  double total = salarioTotal(empleados);
+  double promedio = total/empleados.length;
+
+  return promedio;
 }
